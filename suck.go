2e513e580e41219e43d570fc5b21b250ddf55c28
Zach Latta <zach@zachlatta.com>
2014-05-25 07:21:38 -0600

    Don't visit repeat urls.

--- suck.go
+++ suck.go
@@ -15,7 +15,33 @@ import (
 var (
 	requestsMade = 0
 	urlsFound    = 0
+	urls         = NewURLMap()
 )
+
+type URLMap struct {
+	urlMap map[string]bool
+	mutex  *sync.Mutex
+}
+
+func NewURLMap() *URLMap {
+	return &URLMap{
+		urlMap: map[string]bool{},
+		mutex:  &sync.Mutex{},
+	}
+}
+
+func (u *URLMap) Exists(url string) bool {
+	u.mutex.Lock()
+	exists := u.urlMap[url]
+	u.mutex.Unlock()
+	return exists
+}
+
+func (u *URLMap) Add(url string) {
+	u.mutex.Lock()
+	u.urlMap[url] = true
+	u.mutex.Unlock()
+}
 
 var regexpURL = regexp.MustCompile(`(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s!()\[\]{};:'".,<>?«»“”‘’]))`)
 
@@ -33,7 +59,7 @@ type Sucker struct {
 }
 
 func (s *Sucker) Run() {
-	s.results = make(chan *result, 9999999)
+	s.results = make(chan *result, 99999999)
 	s.run()
 }
 
@@ -50,7 +76,7 @@ func (s *Sucker) run() {
 		}()
 	}
 
-	req, err := http.NewRequest("GET", "http://yahoo.com", nil)
+	req, err := http.NewRequest("GET", "http://wikipedia.org", nil)
 	if err != nil {
 		panic(err)
 	}
@@ -78,18 +104,22 @@ func (s *Sucker) worker(ch chan *http.Request) {
 			}
 
 			links := links(resp.Body)
-			go func(base *url.URL) {
-				urlsFound += len(links)
+			go func(links []*url.URL, base *url.URL) {
 				for _, link := range links {
 					link = base.ResolveReference(link)
-					req, err = http.NewRequest("GET", link.String(), nil)
-					if err != nil {
-						fmt.Println(err)
-					}
 
-					s.jobs <- req
+					if !urls.Exists(link.String()) {
+						urlsFound++
+						urls.Add(link.String())
+						req, err = http.NewRequest("GET", link.String(), nil)
+						if err != nil {
+							fmt.Println(err)
+						}
+
+						s.jobs <- req
+					}
 				}
-			}(req.URL)
+			}(links, req.URL)
 
 			resp.Body.Close()
 		}

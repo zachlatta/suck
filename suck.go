5bc676bdd313245ed3605a1df34407732f886b4f
Zach Latta <zach@zachlatta.com>
2014-06-13 23:03:36 -0700

    Shove results into Neo4j without any relationships.

--- suck.go
+++ suck.go
@@ -6,16 +6,20 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"regexp"
 	"sync"
 	"time"
 
+	"github.com/jmcvetta/neoism"
+
 	"code.google.com/p/go.net/html"
 )
 
 var (
+	db           *neoism.Database
 	requestsMade = 0
 	urlsFound    = 0
 	urls         = NewURLMap()
@@ -44,7 +48,7 @@ type Sucker struct {
 }
 
 func (s *Sucker) Run(firstURL string) {
-	s.results = make(chan *result, 99999999)
+	s.results = make(chan *result)
 	s.run(firstURL)
 }
 
@@ -58,6 +62,38 @@ func (s *Sucker) run(firstURL string) {
 		go func() {
 			s.worker(s.jobs)
 			wg.Done()
+		}()
+	}
+
+	for i := 0; i < 4; i++ {
+		go func() {
+			for res := range s.results {
+
+				referer, e := "", ""
+
+				if res.referer != nil {
+					referer = res.referer.String()
+				}
+
+				if res.err != nil {
+					e = res.err.Error()
+				}
+
+				node, err := db.CreateNode(neoism.Props{
+					"url":           res.url.String(),
+					"referer":       referer,
+					"err":           e,
+					"statusCode":    res.statusCode,
+					"duration":      res.duration,
+					"contentLength": res.contentLength,
+				})
+				if err != nil {
+					log.Println(err)
+				}
+				node.AddLabel("Website")
+
+				fmt.Printf("COUNT: %d, LINKS: %d, REQ RECEIVED: %s\n", requestsMade, urlsFound, res.url.String())
+			}
 		}()
 	}
 
@@ -75,7 +111,6 @@ func (s *Sucker) worker(ch chan *request) {
 	client := &http.Client{}
 	for req := range ch {
 		requestsMade++
-		fmt.Printf("COUNT: %d, LINKS: %d, REQ RECEIVED: %s\n", requestsMade, urlsFound, req.URL.String())
 		start := time.Now()
 		resp, err := client.Do(req.Request)
 		if err != nil {
@@ -117,6 +152,8 @@ func (s *Sucker) worker(ch chan *request) {
 		}
 
 		s.results <- &result{
+			url:           req.URL,
+			referer:       req.referer,
 			statusCode:    code,
 			duration:      time.Now().Sub(start),
 			err:           err,
@@ -147,7 +184,14 @@ func links(reader io.Reader) []*url.URL {
 
 func main() {
 	firstURL := flag.String("first", "http://wikipedia.org", "first url to scrape")
+	dbURL := flag.String("db", "http://localhost:7474/db/data", "url to neo4j")
 	flag.Parse()
+
+	var err error
+	db, err = neoism.Connect(*dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	sucker := Sucker{
 		ConcurrencyLevel: 64,
